47554fe57017850b57f22e6b2ec242a957cfd5d7
opilane <opilane@HP-CZC2349HTZ>
2026-02-04 15:47:37 +0200

    6 Ül

--- Mobiilerakendus/MainPage.xaml.cs
+++ Mobiilerakendus/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Mobiilerakendus
+﻿using System.Threading.Tasks;
+
+namespace Mobiilerakendus
 {
     public partial class MainPage : ContentPage
     {
@@ -17,7 +19,12 @@
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
-                CounterBtn.Text = $"Clicked {count} times";
+                if (count >=5)
+            {
+                CounterBtn.BackgroundColor = Colors.Red;
+                CounterBtn.TextColor = Colors.White;
+            }
+            CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
             var rnd = new Random();
@@ -36,6 +43,56 @@
             CounterBtn.Text = "Vajuta siia";
             CounterLabel.Text = "Alustame uuesti!";
             DotNetBot.Rotation = 0;
+            DotNetBot.IsVisible = true;
+            ResetBtn.ClearValue(BackgroundColorProperty);
+            CounterBtn.ClearValue(BackgroundColorProperty);
+            CounterBtn.ClearValue(Button.TextColorProperty);
+            BackgroundColor = Colors.White;
+            if (DotNetBot.HorizontalOptions == LayoutOptions.Start)
+            {
+                DotNetBot.HorizontalOptions = LayoutOptions.End;
+            }
+            else
+            {
+                DotNetBot.HorizontalOptions = LayoutOptions.Start;
+            }
+        }
+
+        //private void Upbtn_Clicked(object sender, EventArgs e)
+        //{
+        //    pilt.VerticalOptions == LayoutOptions.Start;
+        //}
+
+        //private void Downbtn_Clicked(object sender, EventArgs e)
+        //{
+        //    pilt.VerticalOptions == LayoutOptions.End;
+        //}
+
+        private async void Upbtn_Clicked(object sender, EventArgs e)
+        {
+            await pilt.TranslateToAsync(0, -75, 250);
+        }
+
+        private async void Downbtn_Clicked(object sender, EventArgs e)
+        {
+            await pilt.TranslateToAsync(0, 0, 250);
+        }
+
+        private void Opacity_Clicked(object sender, EventArgs e)
+        {
+            pilt.Opacity -=0.2;
+        }
+
+        private void Scale_Clicked(object sender, EventArgs e)
+        {
+            pilt.Scale += 0.3;
+        }
+
+        private void Cornerrad_Clicked(object sender, EventArgs e)
+        {
+            Opacity.CornerRadius = 40;
+            Scale.CornerRadius = 60;
+            Cornerrad.CornerRadius = 100;
         }
     }
 }
